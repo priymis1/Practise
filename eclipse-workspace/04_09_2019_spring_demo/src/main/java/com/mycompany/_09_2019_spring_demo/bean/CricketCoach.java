79ad970254de56f206973a0e91090a4c6650a2fa
b4ibmjava19@iiht.tech <b4ibmjava19@iiht.tech>
2019-09-04 15:16:10 +0530

    included setter injection

--- eclipse-workspace/04_09_2019_spring_demo/src/main/java/com/mycompany/_09_2019_spring_demo/bean/CricketCoach.java
+++ eclipse-workspace/04_09_2019_spring_demo/src/main/java/com/mycompany/_09_2019_spring_demo/bean/CricketCoach.java
@@ -3,13 +3,59 @@ package com.mycompany._09_2019_spring_demo.bean;
 public class CricketCoach implements Coach {
 	
 	private FortuneService fortuneService;
+	private String email;
+	private String teamName;
 
 	public String getDailyWorkout() {
 		
-		return "practise 5k running today";
+		return "practise 5k running today "+ getTeamName() +" " +getEmail();
 	}
 	
 	
+
+	public CricketCoach() {
+		super();
+	}
+	
+	
+
+
+
+	public FortuneService getFortuneService() {
+		return fortuneService;
+	}
+
+
+
+	public void setFortuneService(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+
+	public String getTeamName() {
+		return teamName;
+	}
+
+
+
+	public void setTeamName(String teamName) {
+		this.teamName = teamName;
+	}
+
+
 
 	public CricketCoach(FortuneService fortuneService) {
 		super();
