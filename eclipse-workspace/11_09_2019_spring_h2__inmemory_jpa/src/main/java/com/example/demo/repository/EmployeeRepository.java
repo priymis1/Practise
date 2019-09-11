6c14294d9e1a598651aa745c77de451420ad3403
b4ibmjava19@iiht.tech <b4ibmjava19@iiht.tech>
2019-09-11 15:39:58 +0530

    Added add employee html

--- eclipse-workspace/11_09_2019_spring_h2__inmemory_jpa/src/main/java/com/example/demo/repository/EmployeeRepository.java
+++ eclipse-workspace/11_09_2019_spring_h2__inmemory_jpa/src/main/java/com/example/demo/repository/EmployeeRepository.java
@@ -1,11 +1,12 @@
 package com.example.demo.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.Employee;
 
 @Repository
-public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
